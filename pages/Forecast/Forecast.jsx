d3cf4fbc3f8633ec5df6c00403e7d3bd9d251607
Kevin <kevin@MacBook-Pro-de-Kevin.local>
2023-07-06 13:40:39 -0400

    ajout du boutton back

--- pages/Forecast/Forecast.jsx
+++ pages/Forecast/Forecast.jsx
@@ -1,14 +1,27 @@
-import { useRoute } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import { Container } from "../../components/Container/Container";
 import { Txt } from "../../components/Txt/Txt";
 import { s } from "./Forecast.style";
+import { TouchableOpacity, View } from "react-native";
 
 export function Forecast({}) {
   const { params } = useRoute();
-  console.log("***", params);
-  return (
-    <Container>
-      <Txt>Forecast</Txt>
-    </Container>
+  const nav = useNavigation();
+
+  const backButton = (
+    <TouchableOpacity style={s.back_btn} onPress={() => nav.goBack()}>
+      <Txt>{"<"}</Txt>
+    </TouchableOpacity>
   );
+
+  const header = (
+    <View style={s.header}>
+      {backButton}
+      <View style={s.header_texts}>
+        <Txt>{params.city}</Txt>
+        <Txt style={s.subtitle}>Prévision sur 7 jours</Txt>
+      </View>
+    </View>
+  );
+  return <Container>{header}</Container>;
 }
